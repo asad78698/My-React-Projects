1a7d3eac39d1f97dddd155c76a11c4402124ac44
Your Name <you@example.com>
2024-02-06 12:16:22 +0500

    myviteproject bgchanger

---
+++ firstreactproject/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1>Ham Titu Ham Titu Ap Bhe Titu Ham Titu!</h1>
+      <p>This is a simple example of how to use react-router with Next. </p>
+    </div>
+    </>
+  )
+}
+
+export default App
